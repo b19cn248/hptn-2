c1c3d4f6bc281b8af9dcfbf3287837616aac616f
Hieu Nguyen <hieu.nguyen@ncsgroup.vn>
2023-10-18 11:34:06 +0700

    init project

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add any interactivity you need here. For example:
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Form submitted!");
+});
